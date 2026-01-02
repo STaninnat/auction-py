1ee8f7088238c1f7e280ae8b5168f53a84eeb58e
STaninnat <taninnat.s@gmail.com>
2026-01-03 00:36:40 +0700

    Infra/observability logging (#4)
    * feat: add Sentry error tracking and structured logging configuration
    
    * feat: implement structured logging and Sentry for the realtime service, and enhance Docker Compose health checks and dependencies
    
    * refactor: rename Sentry DSN environment variables to be service-specific for realtime and core services
    
    ---------
    
    Co-authored-by: STaninnat <taninnat.s@gmailcom>

--- services/realtime/main.py
+++ services/realtime/main.py
@@ -1,17 +1,26 @@
-import logging
-
+import sentry_sdk
+from decouple import config
 from fastapi import FastAPI
+from utils.logger import LoggerSetup
 
-# Basic setup for logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Setup Logging
+logger_setup = LoggerSetup()
+root_logger = logger_setup.configure()
+
+# Setup Sentry
+SENTRY_DSN = config("SENTRY_DSN_REALTIME", default=None)
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        traces_sample_rate=1.0,
+        environment="development" if config("DEBUG", cast=bool) else "production",
+    )
 
 # Create FastAPI app
 app = FastAPI()
 
 
-# Basic route
 @app.get("/")
 async def root():
-    logger.info("Health check endpoint called")
+    root_logger.info("Health check endpoint accessed", extra={"client_ip": "127.0.0.1"})
     return {"message": "Hello form Real-time Service (FastAPI)"}
