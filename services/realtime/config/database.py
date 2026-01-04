f926bb87ed2c842dfed30f9413ba3e421f8b7d9b
STaninnat <taninnat.s@gmail.com>
2026-01-05 05:44:07 +0700

    Infra/async infrastructure (#10)
    * feat: introduce async PostgreSQL database configuration and update dependencies
    
    * feat: add Redis client configuration and setup for the realtime service and remove type ignore comments from models
    
    * refactor: replace  with individual database connection environment variables
    
    * refactor: update SQLAlchemy async session creation and enhance mypy type checking with new dependencies
    
    ---------
    
    Co-authored-by: STaninnat <taninnat.s@gmailcom>

---
+++ services/realtime/config/database.py
@@ -0,0 +1,39 @@
+from decouple import config
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+
+DB_USER = config("DB_USER")
+DB_PASSWORD = config("DB_PASSWORD")
+DB_HOST = config("DB_HOST")
+DB_PORT = config("DB_PORT", default=5432)
+DB_NAME = config("DB_NAME")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Create Engine (The main engine for connection)
+# echo=True will help print the SQL query during development (similar to Django debug log).
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=config("DEBUG", default=False, cast=bool),
+    future=True,
+)
+
+# Create Session (The session for connection)
+# Every time a Request comes in, we will retrieve a Session from here.
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
+
+# Create Base (The base for all models)
+Base = declarative_base()
+
+
+# Dependency Injection Function
+# This function is called by FastAPI to pass a session to the View Function.
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
---
+++ services/realtime/config/redis.py
@@ -0,0 +1,25 @@
+from typing import AsyncGenerator
+
+from decouple import config
+from redis.asyncio import ConnectionPool, Redis
+
+REDIS_HOST = config("VALKEY_HOST", default="valkey")
+REDIS_PORT = config("VALKEY_PORT", default=6379, cast=int)
+REDIS_DB = config("VALKEY_DB", default=0, cast=int)
+
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+
+# Create Connection Pool
+pool = ConnectionPool.from_url(REDIS_URL)
+
+
+async def get_redis() -> AsyncGenerator[Redis, None]:
+    """
+    Dependency Injection for Redis connection
+    """
+
+    client = Redis(connection_pool=pool)
+    try:
+        yield client
+    finally:
+        await client.close()
