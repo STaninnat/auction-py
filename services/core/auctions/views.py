23429f3ba217035ab1605a7b32de250b58042928
STaninnat <taninnat.s@gmailcom>
2026-01-06 22:02:24 +0700

    feat: implement auction listing and user profile views

--- services/core/auctions/views.py
+++ services/core/auctions/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from rest_framework import generics, permissions
+
+from .models import AuctionListing
+from .serializers import AuctionListingSerializer
+
+
+class AuctionListAPIView(generics.ListAPIView):
+    serializer_class = AuctionListingSerializer
+    permission_class = [permissions.AllowAny]
+
+    def get_queryset(self):
+        return AuctionListing.objects.filter(status="ACTIVE").order_by("-created_at")
+
+
+class AuctionRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = AuctionListing.objects.all()
+    serializer_class = AuctionListingSerializer
+    permission_classes = [permissions.AllowAny]
+    lookup_field = "id"
--- services/core/users/views.py
+++ services/core/users/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+
+class MeAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
