cd22d4510b07332168543abc3905bf891c3e8a0a
STaninnat <taninnat.s@gmail.com>
2026-01-06 23:01:13 +0700

    Feat/rest api auctions (#17)
    * feat: implement serializers for user authentication and auction listings
    
    * feat: implement auction listing and user profile views
    
    * feat: add modular API routing for user authentication and auction endpoints
    
    * feat: add basic register endpoint for initial testing
    
    ---------
    
    Co-authored-by: STaninnat <taninnat.s@gmailcom>

--- services/core/users/serializers.py
+++ services/core/users/serializers.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .models import User
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -10,3 +15,31 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token["username"] = user.username
 
         return token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
+        read_only_fields = ["id"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password_confirm")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirm"]:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"], email=validated_data["email"], password=validated_data["password"]
+        )
+        return user
--- services/core/users/views.py
+++ services/core/users/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class MeAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
